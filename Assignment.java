4162bbb048bbb8f309f5f3e4c508261e3d566965
Ugyen Lhamo 12190102 <57824630+Ugyenlhamo@users.noreply.github.com>
2020-10-05 21:04:37 +0600

    Add files via upload

--- Assignment.java
+++ Assignment.java
@@ -1,3 +1,4 @@
+ 
 import java.util.*;
 public class Assignment{
     
@@ -11,25 +12,31 @@ public class Assignment{
 
     //main method
     public static void main(String[] args) {
-        Assignment obj = new Assignment(2);
-        System.out.println("The Size of an array before adding is: "+obj.size());
+        Assignment obj = new Assignment(4);
+
         obj.add(1);
-        obj.add(2);
+        obj.add(1);
+        obj.add(1);
+
+        System.out.println("The Size of an array before adding is: "+obj.size());
+        System.out.println("The Size of an array before adding is: "+obj.size);
+        for (int i=0;i<obj.size ;i++ ) {
+            System.out.println("array is: "+obj.array[i]);
+        }
         
         
         //obj.pop();
         
         obj.resize();
-        obj.pop();
+        //obj.pop();
         System.out.println("The Size of Array is now: "+obj.size());   
     }
 
    
     //Constructor
     public Assignment(int l){
-        max = 4;
-        size = l;
-        count = -1;
+        max = l;
+        size = 0;//2
         array = new int[max];
         onefourth = Math.round((25*l)/100);
         threefourth = Math.round((75*l)/100);
@@ -37,57 +44,52 @@ public class Assignment{
     // adding element in array.
     public void add(int ele){
         if (size==0){
-                array[0]=ele;
-                count = 0;
+            array[size]=ele;
         }
         else{
-            array[count+1]=ele;
-            count = count + 1;
+            array[size+1]=ele;
         }
         size = size+1;
-        System.out.println(" "+ele);
-        System.out.println(" ");
     }
     //removing element in array
     public void pop(){
-        if(count > 0){
-            array[count-1] = 0; 
-            count--; 
+        if(size > 0){
+            array[size-1] = 0; 
+            size--; 
         }
     }
     //Size of array
     public int size(){
-        return size;
+        return max;
     }
     //Resizing array that is if the array is 1/4 it is made into half and if array is 3/4 or full array size is doubled.
     //Array remain same if the array element is 2/4.    
     public void resize(){
       
-        if(count == onefourth){
+        if(size == onefourth){
 
-            int[] temp = new int[size/2];
+            int[] temp = new int[max/2];
 
-            for(int i=0;i<count;i++){
+            for(int i=0;i<size;i++){
                 temp[i]=array[i];
             }
 
-            size = size/2;
+            max = max/2;
             array = temp;
         }
 
-        if(count == threefourth){
+        if(size == threefourth){
 
-            int[] temp = new int[size*2];
+            int[] temp = new int[max*2];
 
-            for(int i=0;i<count;i++){
+            for(int i=0;i<size;i++){
                 temp[i]=array[i];
             }
 
-            size = size*2;
+            max = max*2;
             array = temp;
         } 
        
     }
 
 }
-   
