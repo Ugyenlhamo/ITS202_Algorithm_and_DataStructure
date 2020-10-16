c2b3715c8d2ab61186726d163b54ab4f671e3d6c
Ugyen Lhamo 12190102 <57824630+Ugyenlhamo@users.noreply.github.com>
2020-10-16 09:15:44 +0600

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	// Question 2a
+	int x = 7 * 1 -(5 % 3) * 4 + 9;
+	printf( " %d \n", x);
+
+
+	// Question 2b
+	int y = 9 ^(6 % 2) + 1 * 3;
+	printf( " %d \n", y);
+
+	return 0;
+
+
+}
