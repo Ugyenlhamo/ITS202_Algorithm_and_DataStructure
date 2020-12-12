7b8f6946ee3ee8165cc28dfb7cd7191b6b0142f4
User <12190102.gcit@rub.edu.bt>
2020-12-12 20:59:57 +0600

    LAB WORKS

---
+++ ADS/lab_9/EdgeWeightedGraph.java
@@ -0,0 +1,119 @@
+import java.util.NoSuchElementException;
+public class EdgeWeightedGraph {
+    private final int V;
+    private int E;
+    private LinkedList<Edge>[] adj;
+    
+    /**
+     * Initializes an empty edge-weighted graph with {@code V} vertices and 0 edges.
+     *
+     * @param  V the number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
+    public EdgeWeightedGraph(int V) {
+        
+    }
+
+    /**
+     * Initializes a random edge-weighted graph with {@code V} vertices and <em>E</em> edges.
+     *
+     * @param  V the number of vertices
+     * @param  E the number of edges
+     * @throws IllegalArgumentException if {@code V < 0}
+     * @throws IllegalArgumentException if {@code E < 0}
+     */
+    public EdgeWeightedGraph(int V, int E) {
+        
+    }
+
+    /**  
+     * Initializes an edge-weighted graph from an input stream.
+     * The format is the number of vertices <em>V</em>,
+     * followed by the number of edges <em>E</em>,
+     * followed by <em>E</em> pairs of vertices and edge weights,
+     * with each entry separated by whitespace.
+     *
+     * @param  in the input stream
+     * @throws IllegalArgumentException if {@code in} is {@code null}
+     * @throws IllegalArgumentException if the endpoints of any edge are not in prescribed range
+     * @throws IllegalArgumentException if the number of vertices or edges is negative
+     */
+    
+    /**
+     * Initializes a new edge-weighted graph that is a deep copy of {@code G}.
+     *
+     * @param  G the edge-weighted graph to copy
+     */
+    public EdgeWeightedGraph(EdgeWeightedGraph G) {
+       
+    }
+    /**
+     * Returns the number of vertices in this edge-weighted graph.
+     *
+     * @return the number of vertices in this edge-weighted graph
+     */
+    public int V() {
+        
+    }
+
+    /**
+     * Returns the number of edges in this edge-weighted graph.
+     *
+     * @return the number of edges in this edge-weighted graph
+     */
+    public int E() {
+        
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        
+    }
+
+    /**
+     * Adds the undirected edge {@code e} to this edge-weighted graph.
+     *
+     * @param  e the edge
+     * @throws IllegalArgumentException unless both endpoints are between {@code 0} and {@code V-1}
+     */
+    public void addEdge(Edge e) {
+        
+    }
+
+    /**
+     * Returns the edges incident on vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the edges incident on vertex {@code v} as an Iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Edge> adj(int v) {
+       
+    }
+
+    /**
+     * Returns all edges in this edge-weighted graph.
+     * To iterate over the edges in this edge-weighted graph, use foreach notation:
+     * {@code for (Edge e : G.edges())}.
+     *
+     * @return all edges in this edge-weighted graph, as an iterable
+     */
+    public Iterable<Edge> edges() {
+      
+    }
+
+    /**
+     * Returns a string representation of the edge-weighted graph.
+     * This method takes time proportional to <em>E</em> + <em>V</em>.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency lists of edges
+     */
+    public String toString() {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
